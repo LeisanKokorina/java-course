c9a23d1d623d885ecf459b5f5c76f9b2d5844446
LeisanKokorina <leisanz94@mail.ru>
2017-11-17 03:08:35 +0300

    Task15 is started

---
+++ Homework/Task15/src/ru/itpark/HashSet.java
@@ -0,0 +1,18 @@
+package ru.itpark;
+
+public class HashSet<V> {
+    private HashMap<V, Object> map;
+        Object object = new Object();
+
+
+
+    public HashSet() {
+        map = new HashMap<>();
+    }
+
+    public void add(V v) {
+        map.put(v, object);
+    }
+
+
+}
