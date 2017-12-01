befce73262a3b9da78f2ac4cfe98f77def38ecdb
LeisanKokorina <leisanz94@mail.ru>
2017-12-01 04:12:16 +0300

    task 17

--- Homework/Task17/src/ru/itpark/Finder.java
+++ Homework/Task17/src/ru/itpark/Finder.java
@@ -1,17 +1,18 @@
 package ru.itpark;
 
-import java.util.Arrays;
-
 public class Finder {
-    public int linearSearch(int a[][], int b) {
+    public void linearSearch(int a[][], int b) {
 
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
                 if (a[i][j] == b) {
-                    return i;
+                    Main.isFound = true;
+                    System.out.println("Число находится в " + i + " - ой строке, в " + j + " - ом столбце");
+                    System.out.println("Нашел " + Thread.currentThread().getName());
+
                 }
             }
         }
-        return -1;
+
     }
 }
--- Homework/Task17/src/ru/itpark/Main.java
+++ Homework/Task17/src/ru/itpark/Main.java
@@ -1,14 +1,12 @@
 package ru.itpark;
 
-import sun.nio.ch.ThreadPool;
-
 import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main {
+    static boolean isFound;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -25,19 +23,36 @@ public class Main {
                 matrix[i][j] = rnd.nextInt(10000);
             }
         }
-        System.out.println("Введите число, которое надо найти: ");
-        int numberToFind = scanner.nextInt();
-        if (numberToFind < 0) {
-            System.err.println("В матрице содержатся только положительные числа");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
-        ExecutorService service = Executors.newFixedThreadPool(m);
 
-        Finder finder = new Finder();
+        while (true) {
+            System.out.println("Введите число, которое надо найти: ");
+            int numberToFind = scanner.nextInt();
+            if (numberToFind < 0) {
+                System.err.println("В матрице содержатся только положительные числа");
+            }
+            ExecutorService service = Executors.newFixedThreadPool(5);
 
-        for (int i = 0; i < m; i++) {
+            Finder finder = new Finder();
+
             service.submit(() -> finder.linearSearch(matrix, numberToFind));
+            service.shutdown();
+
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                throw new IllegalArgumentException(e);
+            }
+            if (!isFound) {
+                System.out.println("Такого числа в матрице нет");
+                break;
+            }
+            isFound = false;
         }
-
-
     }
 }
